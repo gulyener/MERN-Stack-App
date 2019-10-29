eb59087b90d61f1c0380c0822dbeb2b6df01fd88
Gul <gulpembeyener@gmail.com>
2019-10-29 17:59:55 +0100

    Create route files with express

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-// Conntect Database
+// Connect Database
 connectDB();
 
 app.get('/', (req, res) => {
   res.send('API Running');
 });
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
