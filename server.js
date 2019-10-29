e360c14292dcfcfd632b593abdb0c4067caf7ba5
Gul <gulpembeyener@gmail.com>
2019-10-29 17:47:10 +0100

    Connect to MongoDB with Mongoose

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Conntect Database
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('API Running');
 });
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
