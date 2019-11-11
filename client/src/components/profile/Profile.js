8093db74af9f16c6ae4f2009570bd0ddc594af83
Gul <gulpembeyener@gmail.com>
2019-11-11 17:00:03 +0100

    Add ProfileEducation & ProfileExperience components

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience.js';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
@@ -30,6 +31,36 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
           <div class="profile-grid my-1">
             <ProfileTop profile={profile}></ProfileTop>
             <ProfileAbout profile={profile} />
+            <div className="profile-exp bg-white pp-2">
+              <h2 className="text-primary">Experience</h2>
+              {profile.experience.length > 0 ? (
+                <Fragment>
+                  {profile.experience.map(experience => (
+                    <ProfileExperience
+                      key={experience._id}
+                      experience={experience}
+                    ></ProfileExperience>
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No experience credentials</h4>
+              )}
+            </div>
+            <div className="profile-edu bg-white pp-2">
+              <h2 className="text-primary">Education</h2>
+              {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map(education => (
+                    <ProfileExperience
+                      key={education._id}
+                      experience={education}
+                    ></ProfileExperience>
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No education credentials</h4>
+              )}
+            </div>
           </div>
         </Fragment>
       )}
