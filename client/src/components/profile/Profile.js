51b00c4fd3c7fd423fe1ea42e66d147eafb8d8c8
Gul <gulpembeyener@gmail.com>
2019-11-11 17:35:47 +0100

    Display Github Repos

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience.js';
 import { getProfileById } from '../../actions/profile';
+import ProfileGithub from './ProfileGithub';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
   useEffect(() => {
@@ -61,6 +62,8 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
                 <h4>No education credentials</h4>
               )}
             </div>
+
+            {profile.githubusername && <ProfileGithub username={profile.githubusername} />}
           </div>
         </Fragment>
       )}
