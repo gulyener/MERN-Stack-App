db8277e018f62e8846a4016597058f972586ff35
Gul <gulpembeyener@gmail.com>
2019-11-12 17:07:04 +0100

    small changes

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -1,5 +1,5 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const ProfileTop = ({
   profile: {
@@ -8,11 +8,9 @@ const ProfileTop = ({
     location,
     website,
     social,
-    user: { name, avatar }
-  }
+    user: { name, avatar },
+  },
 }) => {
-  console.log(social);
-  console.log(website);
   return (
     <div class="profile-top bg-primary p-2">
       <img class="round-img my-1" src={avatar} alt="" />
@@ -58,7 +56,7 @@ const ProfileTop = ({
 };
 
 ProfileTop.propTypes = {
-  profile: PropTypes.object.isRequired
+  profile: PropTypes.object.isRequired,
 };
 
 export default ProfileTop;
